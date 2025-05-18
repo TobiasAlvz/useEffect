b1c0b9e434e9e6587c1fde47cc91bd5fe1cdb77b
Tobias Alves <tobiasjose12@gmail.com>
2025-05-17 23:21:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react"
+
 async function fetchPokemon() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
-  const data = await response.json();
-  return data.results;
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
+  const data = await response.json()
+  return data.results
 }
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  fetchPokemon().then(result => {
+    console.log("Requisição realizada")
+    console.log(result)
+    setPokemon(result)
+  })
+
   return (
     <div className="app">
       <div>
         <h2>Pokémon</h2>
+        {JSON.stringify(pokemon)}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
